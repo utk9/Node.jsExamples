65e6dbd40ab1a5e17898810bd23711e5f37cc829
utk9 <utkarshlamba.2@gmail.com>
2016-04-26 23:10:12 -0400

    Wrote exmaple code to uplaod image and send back to client

--- RESTfulAPI.js
+++ RESTfulAPI.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var fs = require('fs');
 var app = express();
 
 var multiparty = require('multiparty');
@@ -11,13 +12,29 @@ var users = [
 
 ];
 
+var filePath;
+
 app.get('/', function(req, res){
 	res.json(users);
 })
 
 app.get('/users/:id', function(req, res){
 	res.json(users[req.params.id]);
-})
+});
+
+app.get('/getImage/:id', function(req, res){
+	fs.readFile(filePath, function (err, content) {
+            if (err) {
+                res.writeHead(400, {'Content-type':'text/html'})
+                console.log(err);
+                res.end("No such image");    
+            } else {
+                //specify the content type in the response will be an image
+                res.writeHead(200,{'Content-type':'image/png'});
+                res.end(content);
+            }
+        });
+});
 
 
 
@@ -49,13 +66,18 @@ app.post('/', function(req, res){
 		
 	});
 
+	form.on('file', function(name, file){
+		console.log('eyy, its a file');
+		filePath = file.path;
+		console.log("written to: " + filePath);
+	});
+
 	form.on('close', function() {
   		console.log('Upload completed!');
   		if (id != undefined && number != undefined){
   			users.push({name: id, phone: number});
   		}
   		res.end('Received data\n');
-  		res.send();
   	});
 
   	form.on('error', function(err){
