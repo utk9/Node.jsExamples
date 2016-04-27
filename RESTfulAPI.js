725f72811de5271b06662f3e749ba6d10eda713f
utk9 <utkarshlamba.2@gmail.com>
2016-04-27 03:58:00 -0400

    Added mongoDb add and fetch routes

--- RESTfulAPI.js
+++ RESTfulAPI.js
@@ -1,50 +1,80 @@
 var express = require('express');
 var fs = require('fs');
 var app = express();
+var mongoose = require('mongoose');
 
 var multiparty = require('multiparty');
 
-//app.use(express.bodyParser());
-
-var users = [
-	{ name: 'John', phone: 12345},
-	{ name: 'Kelly', phone: 12346} 
-
-];
-
-var filePath;
-
-app.get('/', function(req, res){
-	res.json(users);
-})
-
-app.get('/users/:id', function(req, res){
-	res.json(users[req.params.id]);
+mongoose.connect("mongodb://localhost/UsersDB", function(err){
+	if (err){
+		console.log(err);
+	} else {
+		console.log('Connection Successful');
+	}
 });
 
-app.get('/getImage/:id', function(req, res){
-	fs.readFile(filePath, function (err, content) {
+var userSchema = new mongoose.Schema({
+	name: String,
+	phone: Number,
+	imagePath: String
+});
+
+var User = mongoose.model('User', userSchema);
+
+
+
+app.get('/', function(req, res){
+	User.find(function(err, users){
+		if (err){
+			console.log(err);
+		} else{
+			res.send(users);
+		}
+
+	});
+})
+
+app.get('/getUserData/:id', function(req, res){
+	
+	User.findOne({'_id': req.params.id}, function(err, user){
+		if (err){
+			console.log(err);
+		} else{
+			res.send(user)
+		}
+	});
+});
+
+app.get('/getUserImage/:id', function(req, res){
+
+	User.findOne({'_id': req.params.id}, function(err, user){
+		//console.log("Path is: " + user[0]);
+		if (err){
+			console.log(err);
+		} else{
+			fs.readFile(user.imagePath, function (err, content) {
+
             if (err) {
                 res.writeHead(400, {'Content-type':'text/html'})
                 console.log(err);
                 res.end("No such image");    
             } else {
-                //specify the content type in the response will be an image
                 res.writeHead(200,{'Content-type':'image/png'});
                 res.end(content);
             }
         });
+		}
+	});
+
+	
 });
-
-
 
 app.post('/', function(req, res){
 
-	var id, number;
+	var id, number, path;
 
 	var form = new multiparty.Form();
 	form.on('field', function(name, value){
-		console.log('inside form.on(feild)');
 		res.statusCode = 200;
 		res.write(name + ": "+ value+"\n");
 		if (name === "Name"){
@@ -55,7 +85,6 @@ app.post('/', function(req, res){
 	});
 
 	form.on('part', function(part){
-		console.log('inside form.on(part)');
 		if (!part.filename){
 			part.resume();
 		}
@@ -68,15 +97,30 @@ app.post('/', function(req, res){
 
 	form.on('file', function(name, file){
 		console.log('eyy, its a file');
-		filePath = file.path;
-		console.log("written to: " + filePath);
+		path = file.path;
+		console.log("written to: " + path);
 	});
 
 	form.on('close', function() {
   		console.log('Upload completed!');
   		if (id != undefined && number != undefined){
-  			users.push({name: id, phone: number});
+  			console.log("exec");
+  			//users.push({name: id, phone: number, imagePath: path});
+  			var newUser = new User({
+  				'name': id,
+  				'phone': number,
+  				'imagePath': path
+  			});
+  			newUser.save(function(err, newUser){
+  				if (err){
+  					console.log("Error adding to database");
+  				} else{
+  					console.log("Added to database.");
+  				}
+  			})
   		}
+
+
   		res.end('Received data\n');
   	});
 
@@ -88,3 +132,6 @@ app.post('/', function(req, res){
 });
 
 app.listen(process.env.PORT || 8888);
+
+
+
